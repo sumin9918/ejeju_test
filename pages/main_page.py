59a6eb37f511ca343dfe9a7e389f49090b6f2f16
sumin9918 <104312167+sumin9918@users.noreply.github.com>
2025-03-19 12:04:42 +0900

    [feat] add open ejeju mall test

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,1 +1,13 @@
-# 임시
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+
+class MainPage:
+    URL = "https://mall.ejeju.net/main/index.do"
+
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+    # 메인 페이지 열기
+    def open(self):
+        self.driver.get(self.URL)
